6d65698008824fad138d1beb785d696d11c25b6c
deshika7 <deshika02022005@gmail.com>
2025-09-04 22:33:37 +0530

    commit changes

--- java_full/f10.java
+++ java_full/f10.java
@@ -3,5 +3,7 @@ package java_full;
 public class f10 {
     public static void main(String[] args) {
         System.out.println("/\"Hello, World!\"/");
+         System.out.println("Hello, World!\n how are you?");
+
     }
 }
