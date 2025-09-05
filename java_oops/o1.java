f02e3df98ad470407990fc3b9e9dc4d29d24f343
deshika7 <deshika02022005@gmail.com>
2025-09-05 22:40:07 +0530

    commit changes

---
+++ java_oops/o1.java
@@ -0,0 +1,12 @@
+package java_oops;
+class Animal {
+    void eat() {
+        System.out.println("eating...");
+    }
+}
+public class o1 {
+    public static void main(String[] args) {
+        Animal a = new Animal();
+        a.eat();
+    }
+}
