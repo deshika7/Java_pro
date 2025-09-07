83e175916f8f61d2bd2f070d75c0ac76691ff339
deshika7 <deshika02022005@gmail.com>
2025-09-07 23:08:50 +0530

    commit changes

--- java_oops/o1.java
+++ java_oops/o1.java
@@ -9,11 +9,23 @@ class Dog extends Animal {
         System.out.println("barking...");
     }
 }
+class bird extends Animal{
+    void bark(){
+        System.out.println("....");
+    }
+}
+
+
 public class o1 {
     public static void main(String[] args) {
         Animal a = new Animal();
         a.eat();
         Dog d = new Dog();
         d.bark();
+        d.eat();
+        bird b = new bird();
+        b.bark();
+        b.eat();
+        
     }
 }
