05b873dac38a7a66fcb3e79c53c58dd5dc8fd0e7
deshika7 <deshika02022005@gmail.com>
2025-09-09 22:08:04 +0530

    commit changes

--- java_full/f10.java
+++ java_full/f10.java
@@ -4,6 +4,15 @@ public class f10 {
     public static void main(String[] args) {
         System.out.println("/\"Hello, World!\"/");
          System.out.println("Hello, World!\n how are you?");
+         System.out.println("Hello, \t World!");
+            System.out.println("Hello, \\ World!"); 
+            System.out.println("Hello, \b World!");
+            System.out.println("Hello, \r World!");
+            System.out.println("Hello, \' World!");
+            System.out.println("Hello, \" World!");
+            System.out.println("Hello, \f World!");
+            System.out.println("Hello, \u00A9 World!");
+            System.out.println("Hello, \u00AE World!");
 
     }
 }
