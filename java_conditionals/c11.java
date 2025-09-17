c65d494f8a9bbc797cb2f304646810fe5641bf1b
deshika7 <deshika02022005@gmail.com>
2025-09-17 23:02:15 +0530

    commit changes

--- java_conditionals/c11.java
+++ java_conditionals/c11.java
@@ -7,13 +7,12 @@ public class c11 {
         Scanner sc = new Scanner(System.in);
 
        
-        System.out.print("Enter first number: ");
+        
         int a = sc.nextInt();
 
-        System.out.print("Enter second number: ");
+        
         int b = sc.nextInt();
 
-        System.out.print("Enter third number: ");
         int c = sc.nextInt();
 
        
