419c92234aa33b95a9b65159a08071a68a7035e9
deshika7 <deshika02022005@gmail.com>
2025-09-21 21:17:22 +0530

    commit changes

--- java_conditionals/c6.java
+++ java_conditionals/c6.java
@@ -7,7 +7,7 @@ public class c6 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
-       // System.out.println("Input a year: " + a);
+        System.out.println("Input a year: " + a);
         if(a%4==0 && a%100!=0 || a%400==0) System.out.println("Leap year");
         else System.out.println("Not a Leap year");
         sc.close();
