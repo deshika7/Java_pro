3cf67b7b93cbdb55b7ef51d92ab9d8d1caaf7a5d
deshika7 <deshika02022005@gmail.com>
2025-09-22 22:56:25 +0530

    commit changes

--- java_conditionals/c4.java
+++ java_conditionals/c4.java
@@ -6,8 +6,8 @@ public class c4 {
      public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
-        if(a%3==0) 
-            System.out.println("The number is divisible by 3");
+        if(a%3==0 && a%2==0) //only 3 i gave extra
+            System.out.println("The number is divisible by 3 and 2");
         else
             System.out.println("The number is not divisible by 3 and gives a remainder "+a%3);
         sc.close();
