9384b39944ffcfe14822e8fb1450d9617515c63d
deshika7 <deshika02022005@gmail.com>
2025-09-28 21:22:49 +0530

    commit changes

--- java_basic/ba1.java
+++ java_basic/ba1.java
@@ -3,6 +3,7 @@ package java_basic;
 public class ba1 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("java");
     }
     
 }
