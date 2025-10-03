3a73924b64e99a8a8decbf6de325b6dae18b39b7
deshika7 <deshika02022005@gmail.com>
2025-10-03 21:07:39 +0530

    commit changes

---
+++ java_basic/ba2.java
@@ -0,0 +1,14 @@
+
+package java_basic;
+import java.util.*;
+
+public class ba2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String name = sc.nextLine();
+        System.out.println("Hello, " + name + "!");
+    }
+}
+
